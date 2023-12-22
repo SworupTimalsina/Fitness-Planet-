59775e0132a58e0f61a0d3ac77f8c5b1c570dd75
Aashrit17 <aashrit17038gmail.comgit config --global user.name Aashrit17>
2023-12-22 19:25:02 +0545

    bottom division

--- FitnessPlanetFrontEnd/src/pages/dashboard.tsx
+++ FitnessPlanetFrontEnd/src/pages/dashboard.tsx
@@ -93,7 +93,17 @@ const Dashboard: React.FC = () => {
                     </div>
                 </div>
             </div>
+            <div className="about">
+                <div className="about-buttons">
+                <button>About Us</button>
+                <button>News</button>
+                <button>Privacy Policy</button>
+
+                </div>
+                <label className="copyright">Copyright © 2023 Fitness planet</label>
+            </div>
     </div>
+
 
 );
 };
