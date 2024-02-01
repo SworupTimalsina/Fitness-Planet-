88c85a9a96934c71c85c11291a44bf4b1c72e2e4
princy151 <agrawalprincy151@gmail.com>
2024-02-01 15:55:10 +0545

    Top bar changes

--- FitnessPlanetFrontEnd/src/pages/components/topbar.tsx
+++ FitnessPlanetFrontEnd/src/pages/components/topbar.tsx
@@ -5,13 +5,16 @@ const TopBar: React.FC = () => {
     return (
         <>
             <div className="top-buttons">
-                <a href='#' style={{ backgroundColor: 'black', color: 'white' }} className="profile-button"><b>Profile</b></a>
+
+
                 <div className="buttons">
                     <a href='/dashboard' style={{ backgroundColor: 'black', color: 'white' }}><b>Home</b></a>
                     <a href='/products' style={{ backgroundColor: 'black', color: 'white' }}><b>Products</b></a>
-                    <a href='#' style={{ backgroundColor: 'black', color: 'white' }}><b>Tracking</b></a>
                     <a href='/aboutus' style={{ backgroundColor: 'black', color: 'white' }}><b>About Us</b></a>
                 </div>
+
+                <a href='#' style={{ backgroundColor: 'black', color: 'white' }} className="profile-button"><b>Profile</b></a>
+
             </div>
 
         </>
